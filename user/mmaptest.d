f681a658ef40c76d0252b4ac0e23acc519c9a633
Anekoique <ctolu01@gmail.com>
2025-02-27 23:52:30 +0800

    feat: delete no use

--- user/mmaptest.d
+++
@@ -1,4 +0,0 @@
-user/mmaptest.o: user/mmaptest.c \
- /usr/riscv64-linux-gnu/usr/include/stdc-predef.h kernel/param.h \
- kernel/fcntl.h kernel/types.h kernel/stat.h kernel/riscv.h kernel/fs.h \
- user/user.h
